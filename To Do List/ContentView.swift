9c0c6fab26b80636b2ea8b9d17f7d0a8986e37c6
Michael Essex <123messex@gmail.com>
2021-01-29 10:52:09 -0600

    Added edit and reduced steps through loop

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -8,16 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var things = ["First Thing", "Second Thing", "Third Thing", "Forth Thing", "Fifth Thing"]
     var body: some View {
         NavigationView {
             List {
-                Text("First Thing")
-                Text("Second Thing")
-                Text("Third Thing")
-                Text("Forth Thing")
-                Text("Fifth Thing")
+                ForEach(things, id: \.self) {thing in Text(thing)
+                    
+                }
+                .onMove(perform: { indices, newOffset in
+                    things.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    things.remove(atOffsets: indexSet)
+                })
             }
             .navigationBarTitle("Stuff")
+            .navigationBarItems(leading: EditButton())
         }
     }
 }
